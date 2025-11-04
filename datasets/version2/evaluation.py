284d1d1783f64adf5a6d93068eb132f5ee3ee688
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2025-11-03 21:45:17 -0600

    update eval script

--- datasets/version2/evaluation.py
+++ datasets/version2/evaluation.py
@@ -5,11 +5,14 @@ import json
 import requests
 
 ARGO_USER = ""
-MODEL = "gemini25pro"
+#MODEL = "gemini25pro"
+MODEL = "gpt5"
 
 # --- Load chat templates with placeholder-based questions and answers ---
-chat_templates = templater.load_template("Dataset.json")
+chat_templates = templater.load_template("Dataset_testing.json")
 evaluation_entries = []
+
+idx = 0
 
 # Loop over each Q&A template
 for template in chat_templates:
@@ -19,4 +22,9 @@ for template in chat_templates:
     status_code, llm_response = argo.climrr_query(ARGO_USER, json.dumps(template), MODEL)
     evaluation_entries.append({"reference": reference_response, "llm": llm_response})
 
-templater.save_template(f"Evaluation_{MODEL.upper()}.json", "w", evaluation_entries)
+    idx +=1
+    print(f"Generated {idx} entries")
+
+    templater.append_last_entry(f"Evaluation_{MODEL.upper()}.json", evaluation_entries)    
+
+#templater.save_template(f"Evaluation_{MODEL.upper()}.json", "w", evaluation_entries)
